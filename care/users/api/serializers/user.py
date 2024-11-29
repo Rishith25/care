41444b0fb3c4c6b2778e2bec6b8c0bcde19fef2b
Rishith25 <kamishetty.rishith@gmail.com>
2024-11-29 21:51:31 +0530

    Added Emergency Number UserBaseMinimumSerializer

--- care/users/api/serializers/user.py
+++ care/users/api/serializers/user.py
@@ -388,6 +388,7 @@ class UserBaseMinimumSerializer(serializers.ModelSerializer):
             "user_type",
             "last_login",
             "read_profile_picture_url",
+            "alt_phone_number"
         )
 
 
